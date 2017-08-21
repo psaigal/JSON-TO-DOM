50f7ba3a6385e8fe386c7eef2f6fb97ffbb0fe60
psaigal <psaigal48@gmail.com>
2017-08-21 14:59:24 -0700

    View friends list by clcking on div

--- dom.js
+++ dom.js
@@ -1,9 +1,5 @@
 $(document).ready(function(){
-	(function blah(){
-
-	//GLOBAL VARIABLES
-	// var count = 0;
-	// var maxCount = 10;
+	(function friendList(){
 
 	//CACHE DOM
 	var container = $("#container");
@@ -15,12 +11,25 @@ $(document).ready(function(){
 	
 	function callback(data, status){
 		data = $.parseJSON(data);
+		console.log(status);
 		loadButton.click(function(){
 			getData(data, container);
 		})
+
+		container.on("click", ".person", function(event){
+			if ($(event.target).attr("class") === "person" ){
+				var personInfo = $(event.target);
+				personInfo.children().fadeOut("2000", function(){
+					if ($(personInfo.children()[personInfo.children().length-1]).css("display") === "none"){
+						console.log("qwewqeqwe")
+						getFriends(data, event.target);
+					}
+				});
+			}
+		})
 	}
 
-	var addCount = function() {
+	var addCount = (function addCount() {
 		var count = 0;
 		var maxCount = 10;
 		return function(action){
@@ -33,14 +42,15 @@ $(document).ready(function(){
 				maxCount: maxCount
 			}
 		}
-	}();
+	})();
+
 
 
 	function getData(data,container) {
 		var min = addCount("get").count;
 		var max = addCount("get").maxCount;
 		for (var i = min; (i < max) && (i <= data.length-1); i++) {
-			var $person = $("<div>", {id: "person-" + data[i].id, "class": "person"});
+			var $person = $("<div>", {id: data[i].id, "class": "person"});
 			var personInfo = "<h3>" + data[i].first_name + " " + data[i].last_name  + "</h3>";
 			personInfo += "<p>" + data[i].gender + "</p>";
 			personInfo += "<p>" + data[i].email + "</p>";
@@ -51,6 +61,20 @@ $(document).ready(function(){
 	}
 	})();
 
+	function getFriends(data,person){
+		console.log("ewgfuywevfuewgbfhj")
+		for(var i = 0; i <= data.length-1; i++){
+			if (data[i].id === parseFloat($(person).attr("id"))){
+				var friendInfo = "<p>Friends</p>";
+				data[i].friends.forEach(function(friend){
+					friendInfo += "<p>" +  friend.first_name + " " + friend.last_name + "</p>";
+					friendInfo += "<p>" + friend.email + "</p>";
+				})
+				$(person).append(friendInfo);
+			}
+		}
+
+	}
 })
 
 
