218c0d9c39f86e5e7ce58a04260dada8fbb26b64
psaigal <psaigal48@gmail.com>
2017-08-21 10:51:22 -0700

    Fetch JSON data

--- dom.js
+++ dom.js
@@ -1,10 +1,17 @@
 $(document).ready(function(){
-	console.log("HELLO WORLD");
 
 	//FETCH JSON DATA
-
-
+	$.get("https://dl.dropboxusercontent.com/s/bs8iiggp3ot7g6h/data.json", callback);
+	
+	function callback(data, status){
+		console.log(status);
+		console.log($.parseJSON(data));
+	}
 
 
 
 })
+
+
+
+
