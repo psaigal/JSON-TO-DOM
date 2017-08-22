e9dbdb1d7275bb46771c0b015a789f7c5c6642ed
psaigal <psaigal48@gmail.com>
2017-08-22 11:34:02 -0700

    Add additional styling and error handling

--- dom.js
+++ dom.js
@@ -7,27 +7,31 @@ $(document).ready(function(){
 	
 	//FETCH JSON DATA
 	var data;
-	$.get("https://dl.dropboxusercontent.com/s/bs8iiggp3ot7g6h/data.json", callback);
+	$.get("https://dl.dropboxusercontent.com/s/bs8iiggp3ot7g6h/data.json", callback).fail(function(jqXHR) {
+		container.append("<p>Sorry there was an error retrieving the data from the server</p>")
+	});
 	
 	function callback(data, status){
 		data = $.parseJSON(data);
-		console.log(status);
-		loadButton.click(function(){
-			getData(data, container);
-		})
+		if (status === "success") {
+			loadButton.click(function(){
+				getData(data, container);
+			})
 
-		container.on("click", ".person", function(event){
-			if ($(event.target).attr("class") === "person" ){
-				var personInfo = $(event.target);
-				personInfo.children().fadeOut("2000", function(){
-					if ($(personInfo.children()[personInfo.children().length-1]).css("display") === "none"){
-						console.log("qwewqeqwe")
-						getFriends(data, event.target);
-					}
-				});
-			}
-		})
+			container.on("click", ".person", function(event){
+				if ($(event.target).attr("class") === "person" ){
+					var personInfo = $(event.target);
+					personInfo.children().fadeOut("2000", function(){
+						if ($(personInfo.children()[personInfo.children().length-1]).css("display") === "none"){
+							getFriends(data, event.target);
+						}
+					});
+				}
+			})
+		}
 	}
+
+	//HELPER FUNCTION
 
 	var addCount = (function addCount() {
 		var count = 0;
@@ -52,8 +56,8 @@ $(document).ready(function(){
 		for (var i = min; (i < max) && (i <= data.length-1); i++) {
 			var $person = $("<div>", {id: data[i].id, "class": "person"});
 			var personInfo = "<h3>" + data[i].first_name + " " + data[i].last_name  + "</h3>";
-			personInfo += "<p>" + data[i].gender + "</p>";
-			personInfo += "<p>" + data[i].email + "</p>";
+			personInfo += "<p class='info-style'>Gender: " + data[i].gender + "</p>";
+			personInfo += "<p class='info-style'>Email: " + data[i].email + "</p>";
 			$person.append(personInfo);
 			container.append($person);
 		}
@@ -62,15 +66,21 @@ $(document).ready(function(){
 	})();
 
 	function getFriends(data,person){
-		console.log("ewgfuywevfuewgbfhj")
 		for(var i = 0; i <= data.length-1; i++){
 			if (data[i].id === parseFloat($(person).attr("id"))){
-				var friendInfo = "<p>Friends</p>";
+				var friendHeader = "<h3>" + data[i].first_name + " " +  data[i].last_name + "'s" + "<h3>Friends</h3>";
+				$(person).append(friendHeader);
+				var $friendInfoDiv = [];
+				var y = 0;
 				data[i].friends.forEach(function(friend){
-					friendInfo += "<p>" +  friend.first_name + " " + friend.last_name + "</p>";
-					friendInfo += "<p>" + friend.email + "</p>";
+					$friendInfoDiv[y] = $("<div>", {"class": "friendInfoDiv"});
+					var friendInfo = "<p class='info-style'>" + (y+1) + ". " +  friend.first_name + " " + friend.last_name + "</p>";
+					friendInfo += "<p class='info-style'>" + friend.email + "</p>";
+					$friendInfoDiv[y].append(friendInfo);
+					$(person).append($friendInfoDiv[y]);
+					y += 1;
 				})
-				$(person).append(friendInfo);
+			
 			}
 		}
 
